6d230537f74f8740f7610fdc50490a2bd93b2126
dexoped <118817544+dexoped@users.noreply.github.com>
2025-11-21 19:35:16 +0530

    Fix test failure

--- app/main.js
+++ app/main.js
@@ -7,7 +7,6 @@ const rl = readline.createInterface({
 
 // TODO: Uncomment the code below to pass the first stage
  rl.question("$ ", (answer) => {
-  questionHandler(answer);
-  console.log('${answer}: command not found');
+   console.log('${answer}: command not found');
    rl.close();
  });
