ee6f50e5c181ae627dcd6515df3634a5d227205a
dexoped <118817544+dexoped@users.noreply.github.com>
2025-11-21 20:33:21 +0530

    added type builtin

--- app/main.js
+++ app/main.js
@@ -4,6 +4,8 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+const builtins = ["exit", "echo", "type"];
 // TODO: Uncomment the code below to pass the first stage
 function promptUser() {
   rl.question("$ ", (answer) => {
@@ -16,7 +18,19 @@ if (cmd === "exit") {
     }else if (cmd === "echo") {
         console.log(args.join(" "));
         promptUser();
-    }
+    }else if (cmd === "type") {
+        if (args.length === 0) {
+            console.log("type: missing operand");
+        } else {
+            args.forEach((arg) => {
+                if (builtins.includes(arg)) {
+                    console.log(`${arg} is a shell builtin`);
+                }
+                else {
+                    console.log(`${arg}: not found`);
+                }
+            })}
+          }
     else{
         console.log(`${cmd}: command not found`);
         promptUser();
