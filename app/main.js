d712078102960942c178507b45979728576dfddb
dexoped <118817544+dexoped@users.noreply.github.com>
2025-11-21 20:20:15 +0530

    Fix the failure

--- app/main.js
+++ app/main.js
@@ -4,19 +4,18 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
 // TODO: Uncomment the code below to pass the first stage
 function promptUser() {
   rl.question("$ ", (answer) => {
     const cmd = answer;
-    console.log(`${cmd}: command not found`);
-
+if (cmd === "exit") {
+      rl.close();
+      process.exit(0);
+    }
+    else{
+        console.log(`${cmd}: command not found`);
+        promptUser();
+    }
   });
 }
 promptUser();
-
-rl.on("SIGINT", () => {
-  console.log("\nExiting");
-  rl.close();
-  process.exit(0);
-});
